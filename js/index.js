ce7eebb68bb2f3d2155daaf3b5d798d97933bc84
jinbot <jhjinbhang@gmail.com>
2017-06-08 14:33:41 +0900

    Aboutme page plus

--- js/index.js
+++
@@ -1,7 +0,0 @@
-// $(function(){
-//   var ht =$(window).height();
-//   $('.background_img').css("height",ht+"px");
-//   $( window ).resize(function() {
-//     $('.background_img').css("height",ht+"px");
-//   });
-// });
