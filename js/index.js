bbc8a329337a75e4eae7cdfcaa2c73e58a41af3c
jinbot <jhjinbhang@gmail.com>
2017-06-15 12:24:22 +0900

    idea develop end

---
+++ js/index.js
@@ -0,0 +1,28 @@
+let posts= ["bizcowork","clothup","hackerton","youngsan","srn","timekey","unifoxkr","reactmini","snake"];
+
+$( function() {
+    for(let i=0; i<posts.length; i++){
+      $( "#"+posts[i]+"_dialog" ).dialog({
+        autoOpen: false,
+        resizable: false,
+        modal: true,
+        draggable:false,
+        width:'auto',
+        closeOnEscape: true,
+        show: {
+          effect: "fold",
+          duration: 500,
+        },
+        hide: {
+          effect: "fold",
+          duration: 500,
+        }
+      });
+      $( "#"+posts[i]+"_box" ).on( "click", function() {
+
+        $( "#"+posts[i]+"_dialog" ).dialog( "open" );
+
+      });
+    }
+  }
+);
